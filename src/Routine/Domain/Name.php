2a00d02f193b6ad0c0e32414df7b3b81bd270d9a
diego.rodriguez <diego.rodriguez@deporvillage.com>
2024-03-19 10:43:56 +0100

    create routine domain

---
+++ src/Routine/Domain/Name.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Routine\Domain;
+
+use App\Shared\Domain\StringValueObject;
+
+class Name extends StringValueObject {}
---
+++ src/Routine/Domain/Routine.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Routine\Domain;
+
+final class Routine
+{
+    public function __construct(private readonly Id $id, private readonly Name $name, private readonly Description $description, private readonly Level $level) {}
+
+    public static function create(Id $id, Name $name, Description $description, Level $level): self
+    {
+       return new self($id, $name, $description, $level);
+    }
+
+    public function id(): Id
+    {
+        return $this->id;
+    }
+
+    public function name(): Name
+    {
+        return $this->name;
+    }
+
+    public function description(): Description
+    {
+        return $this->description;
+    }
+
+    public function level(): Level
+    {
+        return $this->level;
+    }
+}
