01c1e139bc77e5ed191e837e99a4cff8c68a48c7
diego.rodriguez <diego.rodriguez@deporvillage.com>
2024-03-25 20:01:40 +0100

    improve object mother with faker

--- tests/Unit/Routine/Domain/RoutineMother.php
+++ tests/Unit/Routine/Domain/RoutineMother.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace FitnessApi\Tests\Unit\Routine\Domain;
 
+use Faker\Factory;
 use FitnessApi\Routine\Domain\Description as Description;
 use FitnessApi\Routine\Domain\Id as Id;
 use FitnessApi\Routine\Domain\Level as Level;
@@ -12,7 +13,19 @@ use FitnessApi\Routine\Domain\Routine as Routine;
 
 final class RoutineMother
 {
-    public static function create(Id $id, Name $name, Description $description, Level $level): Routine {
+    public static function create(?Id $id, ?Name $name, ?Description $description, ?Level $level): Routine {
+        if (is_null($id)){
+            $id = Factory::create()->uuid;
+        }
+        if (is_null($name)){
+            $name = Factory::create()->name;
+        }
+        if (is_null($description)){
+            $description = Factory::create()->streetName;
+        }
+        if (is_null($level)){
+            $level = Factory::create()->randomNumber();
+        }
         return Routine::create($id, $name, $description, $level);
     }
 }
